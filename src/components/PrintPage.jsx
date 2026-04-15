abd33f8bd18758090be5155ebc95a26580cc0fbf
facu.ib <facundoemiliano.21@gmail.com>
2026-04-15 12:44:40 -0300

    Creo que ya esta bien el tamaño

--- src/components/PrintPage.jsx
+++ src/components/PrintPage.jsx
@@ -44,7 +44,7 @@ export default function PrintPage() {
 				window.JsBarcode(`#bc-${i}`, text, {
 					format: "CODE128",
 					lineColor: "#000",
-					width: 1.5,
+					width: 1.2,
 					height: 60,
 					displayValue: true,
 					fontSize: 13,
